8045656c6d09c831adb9346df24d833b78a437c8
Zach Smith <zwsmith27@gmail.com>
2023-07-08 15:28:58 -0700

    fixed membership migration

--- backend/db/migrations/20230707232839-create-membership.js
+++ backend/db/migrations/20230707232839-create-membership.js
@@ -1,5 +1,11 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
+
+let options = {}
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA
+}
+
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Memberships', {
@@ -37,9 +43,10 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       }
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Memberships');
+    options.tableName = "Memberships"
+    await queryInterface.dropTable(options);
   }
 };
