7bc46ab5bd9e690c95144bb8a566d634f9aa889d
Zach Smith <zwsmith27@gmail.com>
2023-07-08 15:50:46 -0700

    created venues table

---
+++ backend/db/models/venue.js
@@ -0,0 +1,64 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Venue extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Venue.belongsTo(models.Group, {
+        foreignKey: 'groupId'
+      })
+    }
+  }
+  Venue.init({
+    groupId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Groups',
+        key: 'id'
+      }
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    state: {
+      type: DataTypes.STRING(2),
+      allowNull: false,
+      validate: {
+        len: [2,2]
+      }
+    },
+    lat: {
+      type: DataTypes.DECIMAL,
+      validate: {
+        isDecimal: true
+      }
+    },
+    lng: {
+      type: DataTypes.DECIMAL,
+      validate: {
+        isDecimal: true
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'Venue',
+    defaultScope: {
+      attributes: {
+        exclude: ['createdAt', 'updatedAt']
+      }
+    }
+  });
+  return Venue;
+};
