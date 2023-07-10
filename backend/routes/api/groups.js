04c62b3d72d23417ecb863c5866578ed136bfdc0
Zach Smith <zwsmith27@gmail.com>
2023-07-10 13:16:30 -0700

    Get all Groups joined or organized by the Current User endpoint added

--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 const { Group, User, GroupImage } = require('../../db/models')
+const { requireAuth } = require('../../utils/auth.js')
 
 router.get('/', async (req, res) => {
   const groups = await Group.findAll({
@@ -37,6 +38,50 @@ router.get('/', async (req, res) => {
   })
 
   res.json(groupList)
+})
+
+router.get('/current', requireAuth, async (req, res) => {
+  const user = await User.findByPk(req.user.id, {
+    include: [
+      {
+        model: Group,
+        as: 'GroupMembership',
+        include: [
+          {
+            model: User,
+            as: 'Members'
+          },
+          {
+            model: GroupImage
+          }
+        ]
+      }
+    ]
+  })
+
+  const groupList = []
+
+  user.GroupMembership.forEach(group => {
+    groupList.push(group.toJSON())
+  })
+
+  groupList.forEach(group => {
+    group.numMembers = group.Members.length
+    group.GroupImages.forEach(image => {
+      if (image.preview === true) {
+        group.previewImage = image.url
+      }
+    })
+    delete group.Membership
+    delete group.Members
+    delete group.GroupImages
+  })
+
+  const groupObj = {
+    Groups: groupList
+  }
+
+  res.json(groupObj)
 })
 
 module.exports = router
