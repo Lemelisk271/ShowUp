eeebf6dc9d1fe3b532a63a17d18f967f37ca5087
Zach Smith <zwsmith27@gmail.com>
2023-07-11 13:36:56 -0700

    added the Edit an Event specified by its id endpoint

--- backend/routes/api/events.js
+++ backend/routes/api/events.js
@@ -1,9 +1,51 @@
 const express = require('express')
 const router = express.Router()
 const { Op } = require('sequelize')
+const { check } = require('express-validator')
 
 const { Event, Group, Venue, User, EventImage } = require('../../db/models')
 const { requireAuth } = require('../../utils/auth.js')
+const { handleValidationErrors } = require('../../utils/validation.js')
+
+const validateEvent = [
+  check('venueId')
+    .custom(async (value) => {
+      if (value !== null) {
+        const venue = await Venue.findByPk(value)
+        if (!venue) {
+          throw new Error("Venue does not exist")
+        }
+      }
+    }),
+  check('name')
+    .exists({checkFalsy: true})
+    .isLength({min: 5})
+    .withMessage("Name must be at least 5 characters"),
+  check('type')
+    .exists({checkFalsy: true})
+    .isIn(["Online", "In person"])
+    .withMessage('Type must be Online or In person'),
+  check('capacity')
+    .exists({checkFalsy: true})
+    .isInt()
+    .withMessage("Capacity must be an integer"),
+  check('price')
+    .exists({checkFalsy: true})
+    .isDecimal()
+    .withMessage("Price is invalid"),
+  check('description')
+    .exists({checkFalsy: true})
+    .withMessage("Description is required"),
+  check('startDate')
+    .exists({checkFalsy: true})
+    .isAfter()
+    .withMessage("Start date must be in the future"),
+  check('endDate')
+    .exists({checkFalsy: true})
+    .isAfter(this.startDate)
+    .withMessage("End date is less than start date"),
+  handleValidationErrors
+]
 
 router.get('/', async (req, res) => {
   const events = await Event.findAll({
@@ -144,6 +186,65 @@ router.post('/:eventId/images', requireAuth, async (req, res, next) => {
     id: image.id,
     url: image.url,
     preview: image.preview
+  }
+
+  res.json(resObj)
+})
+
+router.put('/:eventId', requireAuth, validateEvent, async (req, res, next) => {
+  const event = await Event.findByPk(req.params.eventId, {
+    include: [
+      {
+        model: User,
+        through: {
+          where: {
+            status: 'co-host'
+          }
+        }
+      }
+    ]
+  })
+
+  if(!event) {
+    res.status(404)
+    return res.json({message: "Event couldn't be found"})
+  }
+
+  const group = await Group.findByPk(event.groupId)
+
+  const authUsers = []
+
+  if (group.organizerId === req.user.id) {
+    authUsers.push(req.user.username)
+  }
+
+  event.Users.forEach(user => {
+    authUsers.push(user.username)
+  })
+
+  if (!authUsers.includes(req.user.username)) {
+    const err = new Error('Invalid Authorization')
+    err.status = 403,
+    err.title = 'Invalid Authorization'
+    err.errors = {message: 'You do not have authorization to edit this event.'}
+    return next(err)
+  }
+
+  event.set(req.body)
+
+  await event.save()
+
+  const resObj = {
+    id: event.id,
+    groupId: event.groupId,
+    venueId: event.venueId,
+    name: event.name,
+    type: event.type,
+    capacity: event.capacity,
+    price: event.price,
+    description: event.description,
+    startDate: event.startDate,
+    endDate: event.endDate
   }
 
   res.json(resObj)
