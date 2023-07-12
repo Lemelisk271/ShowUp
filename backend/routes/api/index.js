f6320a80c79cc1e1a4023273904c9419bd70e4a5
Zach Smith <zwsmith27@gmail.com>
2023-07-12 14:19:11 -0700

    added the Delete an Image for an Event endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -5,6 +5,7 @@ const groupsRouter = require('./groups.js')
 const eventsRouter = require('./events.js')
 const venuesRouter = require('./venues.js')
 const groupImageRouter = require('./group-images.js')
+const eventImageRouter = require('./event-images.js')
 const { restoreUser } = require('../../utils/auth.js')
 
 router.use(restoreUser)
@@ -15,6 +16,7 @@ router.use('/groups', groupsRouter)
 router.use('/events', eventsRouter)
 router.use('/venues', venuesRouter)
 router.use('/group-images', groupImageRouter)
+router.use('/event-images', eventImageRouter)
 
 router.post('test', (req, res) => {
   res.json({
