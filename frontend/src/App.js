83295c471ec4a21a78642d64986f13c981fc738d
Zach Smith <zwsmith27@gmail.com>
2023-07-27 21:06:07 -0700

    added the restore user function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,37 @@
 import { Switch, Route } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import LoginFormPage from './components/LoginFormPage'
+import { restoreUser } from './store/session'
 
 function App() {
+  const dispatch = useDispatch()
+  const [isLoaded, setIsLoaded] = useState(false)
+
+  useEffect(() => {
+    const getUser = async () => {
+      await dispatch(restoreUser())
+      setIsLoaded(true)
+    }
+    getUser()
+  }, [dispatch])
+
   return (
-    <main>
-      <Switch>
-        <Route exact path='/'>
-          <h1>Home Page</h1>
-        </Route>
-        <Route path='/login'>
-          <LoginFormPage />
-        </Route>
-        <Route>
-          <h1>Page Not Found</h1>
-        </Route>
-      </Switch>
-    </main>
+    isLoaded && (
+      <main>
+        <Switch>
+          <Route exact path='/'>
+            <h1>Home Page</h1>
+          </Route>
+          <Route path='/login'>
+            <LoginFormPage />
+          </Route>
+          <Route>
+            <h1>Page Not Found</h1>
+          </Route>
+        </Switch>
+      </main>
+    )
   );
 }
 
