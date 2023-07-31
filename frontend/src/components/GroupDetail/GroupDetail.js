76510aa4a5e929cb4a3fed40302825fdb17d9d56
Zach Smith <zwsmith27@gmail.com>
2023-07-31 15:04:35 -0700

    completed the group detail page

--- frontend/src/components/GroupDetail/GroupDetail.js
+++ frontend/src/components/GroupDetail/GroupDetail.js
@@ -2,6 +2,8 @@ import { useDispatch, useSelector } from 'react-redux'
 import { useParams, Link } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import { fetchSingleGroup } from '../../store/groups'
+import EventListItem from '../EventListItem'
+import './GroupDetail.css'
 
 const GroupDetail = () => {
   const dispatch = useDispatch()
@@ -9,9 +11,9 @@ const GroupDetail = () => {
   const [isLoaded, setIsLoaded] = useState(false)
   const [previewImg, setPreviewImg] = useState({})
   const [groupEvents, setGroupEvents] = useState([])
+  const [upcomingEvents, setUpcomingEvents] = useState([])
+  const [pastEvents, setPastEvents] = useState([])
   const group = useSelector(state => state.groups.singleGroup)
-
-  console.log(group)
 
   useEffect(() => {
     const singleGroup = async () => {
@@ -22,6 +24,7 @@ const GroupDetail = () => {
       setIsLoaded(true)
     }
     singleGroup()
+    // eslint-disable-next-line
   }, [dispatch])
 
   useEffect(() => {
@@ -31,29 +34,87 @@ const GroupDetail = () => {
     }
   }, [group, isLoaded])
 
+  useEffect(() => {
+    if (isLoaded) {
+      const newEvents = []
+      const oldEvents = []
+      groupEvents.forEach(event => {
+        const eventDate = new Date(event.startDate)
+        const eventTime = eventDate.getTime()
+        const today = Date.now()
+        if (eventTime >= today) {
+          newEvents.push(event)
+        } else {
+          oldEvents.push(event)
+        }
+      })
+      oldEvents.sort(function(a, b) {
+        return new Date(b.startDate) - new Date(a.startDate)
+      })
+      newEvents.sort(function(a, b) {
+        return new Date(a.startDate) - new Date(b.startDate)
+      })
+      setUpcomingEvents(newEvents)
+      setPastEvents(oldEvents)
+    }
+  }, [groupEvents, isLoaded])
+
   return (
     <div className='groupDetail-main'>
       {isLoaded ? (
         <>
-          <div className='groupDetail-back'>
-            <p>{'<'}</p>
-            <Link to='/groups'>Groups</Link>
-          </div>
-          <div className='groupDetail-header'>
-            <div className='groupDetail-headerImg'>
-              <img src={previewImg.url} alt={`${group.name} Preview Image`} />
+          <div className='groupDetail-top'>
+            <div className='groupDetail-back'>
+              <p>{'<'}</p>
+              <Link to='/groups'>Groups</Link>
             </div>
-            <div className='groupDetail-groupInfo'>
-              <div className='groupDetail-groupText'>
-                <h1>{group.name}</h1>
-                <p>{`${group.city}, ${group.state}`}</p>
-                <p>{`${groupEvents.length} events . ${group.private ? "Private" : "Public"}`}</p>
-                <p>{`Organized by ${group.Organizer.firstName} ${group.Organizer.lastName}`}</p>
+            <div className='groupDetail-header'>
+              <div className='groupDetail-headerImg'>
+                <img src={previewImg.url} alt={group.name} />
               </div>
-              <button>Join this group</button>
+              <div className='groupDetail-groupInfo'>
+                <div className='groupDetail-groupText'>
+                  <h1>{group.name}</h1>
+                  <p>{`${group.city}, ${group.state}`}</p>
+                  <p>{`${groupEvents.length} events . ${group.private ? "Private" : "Public"}`}</p>
+                  <p>{`Organized by ${group.Organizer.firstName} ${group.Organizer.lastName}`}</p>
+                </div>
+                <button>Join this group</button>
+              </div>
             </div>
           </div>
-          <div className='groupDetail-body'></div>
+          <div className='groupDetail-bottom'>
+            <div className='groupDetail-body'>
+              <div className='groupDetail-org'>
+                <h2>Organizer</h2>
+                <p>{`${group.Organizer.firstName} ${group.Organizer.lastName}`}</p>
+              </div>
+              <div className='groupDetail-about'>
+                <h2>What we're about</h2>
+                <p>{group.about}</p>
+              </div>
+              <div className='groupDetail-upcoming'>
+                {upcomingEvents.length ? (
+                  <>
+                  <h2>{`Upcoming Events (${upcomingEvents.length})`}</h2>
+                  {upcomingEvents.map(event => (
+                    <EventListItem key={event.id} event={event} />
+                  ))}
+                  </>
+                ):("")}
+              </div>
+              <div className='groupDetail-past'>
+                {pastEvents.length ? (
+                  <>
+                  <h2>{`Past Events (${pastEvents.length})`}</h2>
+                  {pastEvents.map(event => (
+                    <EventListItem key={event.id} event={event} />
+                  ))}
+                  </>
+                ):("")}
+              </div>
+            </div>
+          </div>
         </>
       ):(
         <h1>Loading</h1>
