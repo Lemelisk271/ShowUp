a50b251681ef8f7f237d13712698915f29bd43aa
Zach Smith <zwsmith27@gmail.com>
2023-08-02 15:53:52 -0700

    update the group form so that only the organizer can update it

--- frontend/src/components/GroupsUpdate/GroupsUpdate.js
+++ frontend/src/components/GroupsUpdate/GroupsUpdate.js
@@ -1,9 +1,14 @@
 import GroupForm from '../GroupForm'
 import { GroupContext } from '../../context/GroupContext'
 import { useContext } from 'react'
+import { useSelector } from 'react-redux'
+import { Redirect } from 'react-router-dom'
 
 const GroupsUpdate = () => {
   const { currentGroup } = useContext(GroupContext)
+  const user = useSelector(state => state.session.user)
+
+  if (user.id !== currentGroup.organizerId) return <Redirect to='/' />
 
   return (
     <GroupForm formType={'update'} group={currentGroup} />
