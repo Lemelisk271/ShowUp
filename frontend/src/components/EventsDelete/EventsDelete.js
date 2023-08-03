ee122d557f48d501c1c5d476bd6b27503bfb8c92
Zach Smith <zwsmith27@gmail.com>
2023-08-03 08:40:27 -0700

    added crud and style for the delete event page

--- frontend/src/components/EventsDelete/EventsDelete.js
+++ frontend/src/components/EventsDelete/EventsDelete.js
@@ -1,14 +1,32 @@
 import { useModal } from '../../context/Modal'
+import { removeEvent } from '../../store/events'
+import { useDispatch } from 'react-redux'
+import { useHistory } from 'react-router-dom'
+import { useState } from 'react'
+import './EventsDelete.css'
 
-const EventsDelete = () => {
+const EventsDelete = ({ eventId }) => {
   const { closeModal } = useModal()
+  const dispatch = useDispatch()
+  const history = useHistory()
+  const [errors, setErrors] = useState({})
+
+  const onDelete = () => {
+    return dispatch(removeEvent(eventId))
+      .then(closeModal)
+      .then(history.push('/'))
+      .catch(async (res) => {
+        const data = res.json()
+        if (data && data.errors) setErrors(data.errors)
+      })
+  }
 
   return(
     <div className="eventDelete">
       <h1>Confirm Delete</h1>
       <h3>Are you sure you want to remove this event?</h3>
       <div className="eventDelete-buttons">
-        <button className="eventDelete-deleteButton">Yes (Delete Event)</button>
+        <button className="eventDelete-deleteButton" onClick={onDelete}>Yes (Delete Event)</button>
         <button className="eventDelete-noButton" onClick={closeModal}>No (Keep Event)</button>
       </div>
     </div>
